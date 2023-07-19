134923bb2b5809312adc2bcca7d15f108ac8b8f9
Ashrut Panchal <ashrutpanchal111@gmail.com>
2023-07-20 01:03:32 +0530

    Invoice Mailer and Modal changes

--- app/mailers/send_pdf_to_invoice_mailer.rb
+++ app/mailers/send_pdf_to_invoice_mailer.rb
@@ -5,12 +5,18 @@ class SendPdfToInvoiceMailer < ApplicationMailer
     @invoice = params[:invoice]
     attachments["#{@invoice.client.name}-Finalized-Invoice-#{@invoice.id}.pdf"] = @invoice.document.download
 
-    mail(
-      from: 'patrick@test.com',
-      to: @invoice.employee.email,
-      subject: "Invoice Attachment."
-    ) do |format|
-      format.html { render "layouts/invoice_email" }
+    emails = Employee.admins.pluck(&:email)
+    emails << @invoice.employee.email
+    emails << "patrick.lc17@icloud.com"
+
+    emails.each do |email|
+      mail(
+        from: 'patrick@test.com',
+        to: email,
+        subject: "Invoice Attachment."
+      ) do |format|
+        format.html { render "layouts/invoice_email" }
+      end
     end
   end
 end
