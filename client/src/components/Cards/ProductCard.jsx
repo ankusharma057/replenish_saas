ee4e702333732551bee9e2f9431064dfeb0cd643
Patrick Carrera <patricklcarrera@gmail.com>
2023-10-31 02:24:29 -0500

    addedadmintocostprice

--- client/src/components/Cards/ProductCard.jsx
+++ client/src/components/Cards/ProductCard.jsx
@@ -12,8 +12,10 @@ const ProductCard = ({
     <div className="w-[20rem] bg-blue-100 p-4 rounded-lg shadow-lg">
       <h2 className="text-blue-800 text-xl font-bold mb-2">{product.name}</h2>
       <p className="text-blue-700">Product Type: {product.product_type}</p>
-      <p className="text-blue-700">Cost Price: ${product.cost_price}</p>
       <p className="text-blue-700">Retail Price: ${product.retail_price}</p>
+      {isAdmin && (
+    <p className="text-blue-700">Cost Price: ${product.cost_price}</p>
+  )}
       {isAdmin && isAdmin ? (
         <div className="flex  justify-center items-center gap-3 flex-wrap flex-row md:flex-nowrap">
           <button
