290dc39c5ba2dbd98d460e42b285e24705d14341
Ashrut Panchal <ashrutpanchal111@gmail.com>
2024-06-08 17:58:10 +0530

    Added Employee Mentor Uniqueness

--- app/models/employee_mentor.rb
+++ app/models/employee_mentor.rb
@@ -3,4 +3,6 @@ class EmployeeMentor < ApplicationRecord
 
   belongs_to :employee, class_name: "Employee", optional: true
   belongs_to :mentor, class_name: "Employee"
+
+  validates_uniqueness_of :employee_id, scope: :mentor_id
 end
