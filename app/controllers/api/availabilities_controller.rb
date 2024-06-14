806394c4d34b53de2bb3b54a7f7fb8acb6765e0b
Ashrut Panchal <ashrutpanchal111@gmail.com>
2024-06-14 13:44:59 +0530

    Get Availability Schedules

--- app/controllers/api/availabilities_controller.rb
+++ app/controllers/api/availabilities_controller.rb
@@ -2,6 +2,14 @@ class Api::AvailabilitiesController < ApplicationController
   include AvailabilityConcern
   skip_before_action :authorized_employee
 
+  def index
+    if params[:employee_id].present? && params[:location_id].present?
+      render json: Availabilities.employee_locations_availabilities(params)
+    else
+      render json: { error: 'employee_id and location_id are required' }, status: :unprocessable_entity
+    end
+  end
+
   def create
     create_availability
   end
--- app/models/availabilities.rb
+++ app/models/availabilities.rb
@@ -2,5 +2,9 @@ class Availabilities < ApplicationRecord
   belongs_to :employee_location
   has_many :availability_timings, dependent: :destroy
 
-  accepts_nested_attributes_for :availability_timings 
+  accepts_nested_attributes_for :availability_timings
+
+  def self.employee_locations_availabilities(params)
+    where(employee_location_id: EmployeeLocation.where(employee_id: params[:employee_id], location_id: params[:location_id]).pluck(:id))
+  end
 end
