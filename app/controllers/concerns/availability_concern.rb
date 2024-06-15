abfa9780ecaa69bea5bf3b25dd5c0458416ac118
Ashrut Panchal <ashrutpanchal111@gmail.com>
2024-06-15 22:25:48 +0530

    Support for update_all_my_locations field on availabilities update

--- app/controllers/concerns/availability_concern.rb
+++ app/controllers/concerns/availability_concern.rb
@@ -2,21 +2,31 @@ module AvailabilityConcern
   extend ActiveSupport::Concern
 
   def create_or_update_availability
-    create_or_update_availability_with_timings
+    availabilities_response = []
+    if availability_params[:update_all_my_locations]
+      employee = Employee.find_by(id: availability_params[:employee_id])
+
+      employee.locations.each do |location|
+        availabilities_response += create_or_update_availability_with_timings(location.id)
+      end
+    else
+      availabilities_response += create_or_update_availability_with_timings(availability_params[:location_id])
+    end
+
     render json: { success: true }
   rescue StandardError => e
     render json: { error: e.message }, status: :unprocessable_entity
   end
 
   def availability_params
-    params.require(:availability).permit(:employee_id, :location_id, :start_date, :end_date, 
-                                           availability_timings: [:day, timings: [:start_time, :end_time]])
+    params.require(:availability).permit(:employee_id, :location_id, :start_date, :end_date, :update_all_my_locations,
+                                          availability_timings: [:day, timings: [:start_time, :end_time]])
   end
 
-  def create_or_update_availability_with_timings
+  def create_or_update_availability_with_timings(location_id)
     params_with_employee_location = availability_params
     employee_location = EmployeeLocation.find_by(employee_id: params_with_employee_location[:employee_id],
-                                                 location_id: params_with_employee_location[:location_id])
+                                                 location_id: location_id)
 
     unless employee_location
       raise StandardError.new('Employee location not found')
