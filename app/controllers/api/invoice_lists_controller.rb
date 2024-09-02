ac2dde0b027c53479d52c5157c92f4575b084469
Ashrut Panchal <ashrutpanchal111@gmail.com>
2024-09-02 23:42:49 +0530

    Fixes

--- app/controllers/api/invoice_lists_controller.rb
+++ app/controllers/api/invoice_lists_controller.rb
@@ -1,7 +1,14 @@
 class Api::InvoiceListsController < ApplicationController
-
   def index
-    invoices = Invoice.all
+    invoices = Invoice.includes(
+      :products, 
+      :employee, 
+      :client, 
+      :before_images_attachments, 
+      :after_images_attachments, 
+      :invoice_group
+    ).all
+    
     render json: invoices, each_serializer: InvoiceListSerializer, status: :ok
   end
 end
