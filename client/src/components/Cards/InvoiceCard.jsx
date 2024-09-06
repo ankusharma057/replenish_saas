3d24f3f574b2e40d12e284c1154088cb59aefc3e
Ashrut Panchal <ashrutpanchal111@gmail.com>
2024-09-06 10:09:21 +0530

    Delete Finalized Invoices

--- client/src/components/Cards/InvoiceCard.jsx
+++ client/src/components/Cards/InvoiceCard.jsx
@@ -1,7 +1,10 @@
 import React, { memo } from "react";
 import { Button, Card } from "react-bootstrap";
+import { confirmAlert } from "react-confirm-alert";
+import { toast } from "react-toastify";
+import { deleteInvoice } from "../../Server";
 
-const InvoiceCard = ({ invoice, seeMore, finalizeInvoiceSubmit }) => {
+const InvoiceCard = ({ invoice, seeMore, finalizeInvoiceSubmit, getInvoices }) => {
   return (
     <Card className="text-center" border="info" style={{ width: "18rem" }}>
       <Card.Header as="h5">{invoice.source_invoice_id ? "Mentor " : ""}Invoice Id {invoice.id}</Card.Header>
@@ -25,6 +28,41 @@ const InvoiceCard = ({ invoice, seeMore, finalizeInvoiceSubmit }) => {
           className="text-white"
         >
           Finalize Invoice
+        </Button>
+        <Button
+          style={{ display: invoice.is_finalized ? "inline" : "none" }}
+          onClick={async () => {
+            confirmAlert({
+              title: "Confirm to delete",
+              message: "Are you sure you want to delete this invoice?",
+              buttons: [
+                {
+                  label: "Yes",
+                  onClick: async () => {
+                    try {
+                      const {data} = await deleteInvoice(invoice?.id)
+                      if(data){
+                        toast.success(`Invoice Deleted Successfully.`);
+                        getInvoices(true);
+                      }else{
+                        toast.error("Something went wrong")
+                      }
+                    } catch (error) {
+                      toast.error("Something went wrong")
+                    }
+                  },
+                },
+                {
+                  label: "No",
+                  onClick: () => console.log("Click No"),
+                },
+              ],
+            });
+          }}
+          variant="danger"
+          className="text-white"
+        >
+        Delete
         </Button>
       </Card.Body>
     </Card>
