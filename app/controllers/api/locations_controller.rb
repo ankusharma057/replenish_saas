4619607a4718b3753f704d8eb431f108af615c3c
Ashrut Panchal <ashrutpanchal111@gmail.com>
2024-12-19 14:29:50 +0530

    Added attributes to location

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -7,20 +7,26 @@ class Api::LocationsController < ApplicationController
 
   def create
     location = Location.find_or_create_by(name: params[:location])
-
     if location
-      location.employees = Employee.where(id: params[:employee_ids])
-
+      location.update(location_params)
+      location.employees = Employee.where(id: params[:employee_ids]) if params[:employee_ids]
       render json: location, status: :created
     else
-      render json: {error: 'Something went wrong!'}, status: :unprocessable_entity
+      render json: { error: 'Something went wrong!' }, status: :unprocessable_entity
     end
   end
+
 
   def employees
     location = Location.find_by(id: params[:id])
     employees = location.employees
 
     render json: employees, status: :ok
+  end
+
+  private
+  def location_params
+    params.permit(:short_description, :long_description, :email, :phone_number, :fax, :street_address, :apartment, :city, :country, :province, :postal_code, :display_location_address, :display_map_in_emails, :legal_name, :business_number, :use_location_for_billing, :online_booking_available
+    )
   end
 end
