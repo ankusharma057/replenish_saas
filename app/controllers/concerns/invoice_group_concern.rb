8b29bbdd2955d096cb23638a894310f0e9176cd5
Ashrut Panchal <ashrutpanchal111@gmail.com>
2025-01-31 13:15:22 +0530

    Refactor client retrieval logic for admin

--- app/controllers/concerns/invoice_group_concern.rb
+++ app/controllers/concerns/invoice_group_concern.rb
@@ -11,8 +11,9 @@ module InvoiceGroupConcern
   def create_invoice_group
     @invoice_group = InvoiceGroup.create!
     params['_json'].each do |invoice_param|
-      client = @employee.clients.find_by(name: invoice_param['clientname']) ||
-         @employee.clients.create(
+      client = @employee.is_admin? ? Client.all : @employee.clients
+      client = client.find_by(name: invoice_param['clientname']) ||
+         client.create(
            name: invoice_param['clientname'],
            last_name: invoice_param['lastname'],
            email: invoice_param['email']
